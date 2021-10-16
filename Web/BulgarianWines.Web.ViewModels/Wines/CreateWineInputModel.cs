b23f66ad70ba99b74ade9e07e64c84b1439e2a3a
Videnoff <ivan.videnov@hotmail.com>
2021-10-16 23:25:23 +0300

    Add tynimce to Edit

--- Web/BulgarianWines.Web.ViewModels/Wines/CreateWineInputModel.cs
+++ Web/BulgarianWines.Web.ViewModels/Wines/CreateWineInputModel.cs
@@ -43,12 +43,16 @@
         [ImageAttribute]
         public IEnumerable<IFormFile> UploadedImages { get; set; }
 
+        [Display(Name = "Is Deleted?")]
         public bool IsDeleted { get; set; }
 
+        [Display(Name = "Deleted on")]
         public DateTime? DeletedOn { get; set; }
 
+        [Display(Name = "Created on")]
         public DateTime? CreatedOn { get; set; }
 
+        [Display(Name = "Modified on")]
         public DateTime? ModifiedOn { get; set; }
 
         public IEnumerable<KeyValuePair<string, string>> CategoriesItems { get; set; }
