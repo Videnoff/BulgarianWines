76e805ce18af1330572b8e98b72850fe823b80a3
Videnoff <ivan.videnov@hotmail.com>
2021-10-31 23:35:26 +0200

    add fixed top

--- Web/BulgarianWines.Web.ViewModels/Wines/WinesListViewModel.cs
+++ Web/BulgarianWines.Web.ViewModels/Wines/WinesListViewModel.cs
@@ -2,8 +2,22 @@
 {
     using System.Collections.Generic;
 
-    public class WinesListViewModel : PagingViewModel
+    using AutoMapper;
+    using BulgarianWines.Data.Models;
+    using BulgarianWines.Services.Mapping;
+
+    public class WinesListViewModel : PagingViewModel, IHaveCustomMappings
     {
+        public string CategoryName { get; set; }
+
         public IEnumerable<AllWinesViewModel> Wines { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Category, WinesListViewModel>()
+                .ForMember(x => x.CategoryName, opt =>
+                    opt.MapFrom(x =>
+                        x.Name));
+        }
     }
 }
