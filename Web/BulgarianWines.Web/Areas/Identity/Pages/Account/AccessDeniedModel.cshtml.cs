6f28192c6260ee3e483edae3b5a300c54a6a492a
Videnoff <ivan.videnov@hotmail.com>
2021-12-24 00:29:00 +0200

    Modify no access view and add Users and Roles tabs to AdminLayout

---
+++ Web/BulgarianWines.Web/Areas/Identity/Pages/Account/AccessDeniedModel.cshtml.cs
@@ -0,0 +1,12 @@
+﻿namespace BulgarianWines.Web.Areas.Identity.Pages.Account
+{
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
