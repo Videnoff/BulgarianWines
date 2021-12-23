fc722db37eea289ba8dc49edd95ee789104160df
Videnoff <ivan.videnov@hotmail.com>
2021-12-23 23:26:55 +0200

    Add role editing

---
+++ Web/BulgarianWines.Web.ViewModels/Administration/Users/EditRoleViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace BulgarianWines.Web.ViewModels.Administration.Users
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class EditRoleViewModel
+    {
+        public EditRoleViewModel()
+        {
+            this.Users = new List<string>();
+        }
+
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Role Name is required")]
+        public string RoleName { get; set; }
+
+        public List<string> Users { get; set; }
+    }
+}
--- Web/BulgarianWines.Web/Areas/Administration/Controllers/UsersController.cs
+++ Web/BulgarianWines.Web/Areas/Administration/Controllers/UsersController.cs
@@ -1,11 +1,10 @@
-﻿using BulgarianWines.Web.ViewModels.Administration.Users;
-
-namespace BulgarianWines.Web.Areas.Administration.Controllers
+﻿namespace BulgarianWines.Web.Areas.Administration.Controllers
 {
     using System.Threading.Tasks;
 
     using BulgarianWines.Data.Common.Repositories;
     using BulgarianWines.Data.Models;
+    using BulgarianWines.Web.ViewModels.Administration.Users;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
@@ -79,6 +78,63 @@ namespace BulgarianWines.Web.Areas.Administration.Controllers
         {
             var roles = this.roleManager.Roles;
             return this.View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await this.roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                this.ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found!";
+                return this.NotFound();
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name,
+            };
+
+            foreach (var user in this.userManager.Users)
+            {
+                if (await this.userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await this.roleManager.FindByIdAsync(model.Id);
+
+            if (role == null)
+            {
+                this.ViewBag.ErrorMessage = $"Role with Id = {model.Id} cannot be found!";
+                return this.NotFound();
+            }
+            else
+            {
+                role.Name = model.RoleName;
+                var result = await this.roleManager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return this.RedirectToAction("ListRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    this.ModelState.AddModelError(string.Empty, error.Description);
+                }
+
+                return this.View(model);
+            }
         }
     }
 }
